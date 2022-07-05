236daea1b0ab924ae8b2f5e3d25c990e9d49d239
Rafael Freitas <vidarafael@hotmail.com>
2022-07-05 14:39:14 -0300

    feature('test's from integration')

--- src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -7,7 +7,7 @@ import createConnection from "@shared/infra/typeorm"
 import { Connection } from "typeorm";
 
 let connection: Connection;
-describe("Create Category Controller", () => {
+describe("List Category Controller", () => {
   beforeAll(async () => {
     connection = await createConnection()
     await connection.runMigrations()
@@ -28,7 +28,7 @@ describe("Create Category Controller", () => {
     await connection.close()
   })
 
-  it("should be able to create a new category", async () => {
+  it("should be able to list all categories", async () => {
     const responseToken = await request(app).post("/sessions")
       .send({
         email: "admin@rentx.com.br",
@@ -37,7 +37,7 @@ describe("Create Category Controller", () => {
 
     const { token } = responseToken.body
 
-    const response = await request(app)
+    await request(app)
       .post("/categories")
       .send({
         name: "Category Supertest",
@@ -47,29 +47,12 @@ describe("Create Category Controller", () => {
         Authorization: `Bearer ${token}`
       })
 
-    expect(response.status).toBe(201)
-  })
+    const response = await request(app).get("/categories")
 
-  it("should not be able to create a new category with name exists", async () => {
-    const responseToken = await request(app).post("/sessions")
-      .send({
-        email: "admin@rentx.com.br",
-        password: "admin"
-      })
+    console.log(response.body)
 
-    const { token } = responseToken.body
-
-    const response = await request(app)
-      .post("/categories")
-      .send({
-        name: "Category Supertest",
-        description: "Category Supertest",
-      })
-      .set({
-        Authorization: `Bearer ${token}`
-      })
-
-    expect(response.status).toBe(400)
+    expect(response.status).toBe(200)
+    expect(response.body.length).toBe(1)
   })
 
 })
